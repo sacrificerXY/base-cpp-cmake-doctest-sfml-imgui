508eb1b6756c1311d9a6c27209b59b7ab4cdf034
sacrificerXY <sacrificerxy@gmail.com>
2020-03-18 03:44:15 +0800

    Add working sample test using doctest

--- tests/test-main.cpp
+++ tests/test-main.cpp
@@ -1,8 +1,12 @@
-#include <iostream>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
 #include <_sample/_sample.h>
 
-int main() {
-    foo f(10);
-    f.add(5);
-    std::cout << "TEST " << f.bar << '\n';
+int factorial(int number) { return number <= 1 ? number : factorial(number - 1) * number; }
+
+TEST_CASE("foo test") {
+    foo f(5);
+    CHECK(f.bar == 5);
+    f.add(3);
+    CHECK(f.bar == 8);
 }
