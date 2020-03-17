ac5fbce3d988ca3534b9241b49e38d4394b75aac
sacrificerXY <sacrificerxy@gmail.com>
2020-03-18 05:08:50 +0800

    Add working SFML sample project
    - Add git init/update for SFML
    - Fix type in SFML path in .gitmodules

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
 #include <_sample/_sample.h>
+#include <SFML/Graphics.hpp>
 
-int main() {
-    foo f(10);
-    f.add(5);
-    std::cout << "Sample Project " << f.bar << '\n';
+int main()
+{
+    foo f(50);
+    f.add(50);
+
+    sf::RenderWindow window(sf::VideoMode(f.bar * 2, f.bar * 2), "SFML works!");
+    sf::CircleShape shape(f.bar);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }
